71d53619be166bceffa60b5e831e57ac5521519b
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-08-05 00:24:49 +0530

    Create 3x3.py

---
+++ 3x3.py
@@ -0,0 +1,24 @@
+def read(mat):
+    for i in range(3):
+        a=[]
+        for j in range(3):
+            a.append(int(input()))
+        mat.append(a)
+
+def matrix(mat):
+    print('the matrix is ')
+    for i in range(3):
+        for j in range(3):
+            print(mat[i][j],end=' ')
+        print()
+
+
+def main():
+    mat1 = []
+    print('enter the elements of matrix: ')
+    read(mat1)
+    matrix(mat1)
+
+if __name__=='__main__':
+    main()
+
